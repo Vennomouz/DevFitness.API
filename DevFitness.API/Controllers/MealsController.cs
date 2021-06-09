0438458147f19685503007d45b054397a97772c2
julio <julio@DESKTOP-QTDGH0L>
2021-06-09 16:53:44 -0300

    Adicionar arquivos de projeto.

---
+++ DevFitness.API/Controllers/MealsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DevFitness.API.Models.InputModels;
+using DevFitness.API.Persistence;
+using System.Linq;
+using DevFitness.API.Models.ViewModels;
+using DevFitness.API.Core.Entities;
+
+namespace DevFitness.API.Controllers
+{
+    // api/users/4/meals
+    [Route("api/users/{userId}/meals")]
+    public class MealsController : ControllerBase
+    {
+        private readonly DevFitnessDBContext _dbContext;
+        public MealsController(DevFitnessDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        //api/users/4/meals HTTP GET
+        [HttpGet]
+        public IActionResult GetAll(int userId)
+        {
+            var userMeals =_dbContext.Meals.Where(m => m.UserId == userId && m.Active);
+
+            if(userMeals == null)
+            {
+                return NotFound();
+            }
+
+            var allMealViewModel = userMeals.Select(m => new MealViewModel(m.Id, m.Description, m.Calories, m.Date));
+            return Ok(allMealViewModel);
+        }
+
+        //api/users/4/meals/7 HTTP GET Meal 7 from User 4
+        [HttpGet("{mealId}")]
+        public IActionResult Get(int userId, int mealId)
+        {
+            var userMeal = _dbContext.Meals.SingleOrDefault(m => m.Id == mealId && m.UserId == userId);
+
+            if(userMeal == null)
+            {
+                return NotFound();
+            }
+
+            var mealViewModel = new MealViewModel(userMeal.Id, userMeal.Description, userMeal.Calories, userMeal.Date);
+            return Ok(mealViewModel);
+        }
+
+        //api/users/4/meals HTTP POST
+        [HttpPost]
+        public IActionResult Post(int userId, [FromBody] CreateMealInputModel inputMeal)
+        {
+            var meal = new Meal(inputMeal.Description, inputMeal.Calories, inputMeal.Date, userId);
+
+            _dbContext.Meals.Add(meal);
+            _dbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(Get), new { userId = userId, mealId = meal.Id}, inputMeal);
+        }
+
+        //api/users/4/meals/8 HTTP PUT atualizar meal 8 from user 4
+        [HttpPut("{mealId}")]
+        public IActionResult Put(int userId, int mealId, [FromBody] UpdateMealInputModel updateMeal)
+        {
+            var meal = _dbContext.Meals.SingleOrDefault(m => m.Id == mealId && m.UserId == userId);
+
+            if(meal == null)
+            {
+                return NotFound();
+            }
+
+            meal.Update(updateMeal.Description, updateMeal.Calories, updateMeal.Date);
+
+            _dbContext.Meals.Update(meal);
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{mealId}")]
+        public IActionResult Delete(int userId, int mealId)
+        {
+            var meal = _dbContext.Meals.SingleOrDefault(m => m.Id == mealId && m.UserId == userId);
+
+            if(meal == null)
+            {
+                return NotFound();
+            }
+
+            meal.Deactivate();
+
+            return NoContent();
+        }
+    }
+}
---
+++ DevFitness.API/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+﻿using DevFitness.API.Core.Entities;
+using DevFitness.API.Models.InputModels;
+using DevFitness.API.Models.ViewModels;
+using DevFitness.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFitness.API.Controllers
+{
+    // api/users -> [controller] pega o nome da controller e tira a palavra "controller"
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        //private readonly DevFitnessDBContext dbContexto = new DevFitnessDBContext();
+
+        private readonly DevFitnessDBContext _dbContext;
+        public UsersController(DevFitnessDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = _dbContext.Users.SingleOrDefault(u => u.Id == id);
+
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            var userViewModel = new UserViewModel(user.Id, user.FullName, user.Height, user.Weight, user.BirthDate);
+            return Ok(userViewModel);
+        }
+
+        //api/users método HTTP POST
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateUserInputModel inputUser)
+        {
+            var user = new User(inputUser.FullName, inputUser.Height, inputUser.Weight, inputUser.BirthDate);
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+
+            //return BadRequest();
+
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, inputUser);
+
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateUserInputModel updateUser)
+        {
+            var user = _dbContext.Users.SingleOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Update(updateUser.Height, updateUser.Weight);
+
+            _dbContext.Users.Update(user); // NAO PRECISARIA DE CHAMAR O METODO UPDATE, POIS O CONTEXTO JA ESTA SENDO RASTREADO(TRACKING)
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
